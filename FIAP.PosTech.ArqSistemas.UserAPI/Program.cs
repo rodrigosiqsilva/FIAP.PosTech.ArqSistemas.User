7fa3bab777cbbfa755e4959b5f6259edf0ca9839
Rodrigo Siqueira Silva <rodrigosiqueirasilva@hotmail.com>
2026-06-12 07:10:53 -0300

    Ajustes para execução

--- FIAP.PosTech.ArqSistemas.UserAPI/Program.cs
+++ FIAP.PosTech.ArqSistemas.UserAPI/Program.cs
@@ -3,15 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Configure OpenAPI/Swagger using Swashbuckle
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Register Swagger generator and explorer so the Swagger UI is available at /swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // Enable Swagger middleware and UI in Development
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
